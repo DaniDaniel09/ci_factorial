7f77ec0461829e950476a38294387c3a646c911a
DaniDaniel09 <danielzanon2004@ieseljust.com>
2024-02-16 13:40:52 +0100

    Docstring al metodo factorial

--- src/factorial.py
+++ src/factorial.py
@@ -1,4 +1,26 @@
+"""
+Este módulo contiene una implementación de la función factorial. (actualmente incompleta a mas numeros)
+"""
+
+
 def factorial(valor):
+    """
+    Calcula el factorial de un número entero positivo.
+
+    Esta función devuelve el factorial de un número entero positivo. 
+    Actualmente, solo se admiten los valores 1 y 5, para los cuales 
+    devuelve los factoriales correspondientes (1 y 120, respectivamente).
+
+    Parámetros:
+    valor (int): El número entero del cual se calculará el factorial.
+
+    Devoluciones:
+    int: El factorial del número de entrada.
+
+    Excepciones:
+    ValueError: Si el valor es un entero pero no es mayor que 0.
+    TypeError: Si el valor no es un entero.
+    """
     if isinstance(valor, int):
         if valor > 0:
             if valor == 1:
